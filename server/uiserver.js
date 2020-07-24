2c53a8fd307da4fdd37407f8e187d6ad0dd99811
Tran Pham <pham.tr@husky.neu.edu>
2020-07-23 21:47:20 -0400

    adding port

--- server/uiserver.js
+++ server/uiserver.js
@@ -62,7 +62,7 @@ app.get('*', (req, res, next) => {
   render(req, res, next);
 });
 
-const port = process.env || 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`UI started on port ${port}`);
